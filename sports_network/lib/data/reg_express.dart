349ab340493aba33439e3304a6a1a4bf19e0e03a
Joshua Eze <jezeadiebuo5@gmail.com>
2023-04-17 06:44:06 +0100

    added countrycode api

--- sports_network/lib/data/reg_express.dart
+++ sports_network/lib/data/reg_express.dart
@@ -3,6 +3,6 @@ class RegExpressions {
   static RegExp emailPattern =
       RegExp(r'(^[0-9a-zA-Z_.]+\@[a-z]+\.[a-z]+(\.[a-z]+)?$)');
   static RegExp passwordPattern = RegExp(r'([0-9a-zA-Z_.@$^&]{1,12})');
-  static RegExp phoneNoPattern =
-      RegExp(r'(^\+\d{1,3}\s?\d{1,4}(\-\s)?\d{1,3}(\-\s)?\d{1,4}$)');
+  // static RegExp phoneNoPattern =
+  //     RegExp(r'(^\+\d{1,3}\s?\d{1,4}(\-\s)?\d{1,3}(\-\s)?\d{1,4}$)');
 }
